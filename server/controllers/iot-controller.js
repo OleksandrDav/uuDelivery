4ccefcbc57a98e0378428f5ac3be5ac8cf871721
Oleksandr Davydovskyi <oleksandr.davydovskyi@plus4u.net>
2024-03-14 15:27:41 +0100

    optional iot data

--- server/controllers/iot-controller.js
+++ server/controllers/iot-controller.js
@@ -3,10 +3,12 @@ class IotController {
    async getIotData(req, res) {
       try {
          const iotData = req.body;
-         const encodedData = `${iotData.data}`;
-         const decodedData = atob(encodedData);
+         const encodedData = `${iotData?.data}`;
+         if (encodedData) {
+            const decodedData = atob(encodedData);
+            console.log(decodedData);
+         }
 
-         console.log(decodedData);
          res.status(200).json(iotData);
       } catch (error) {
          console.log(error);
