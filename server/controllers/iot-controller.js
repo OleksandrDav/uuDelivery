f878f2f2de5b723466cdc9f2e9021b9eb89922b4
Oleksandr Davydovskyi <oleksandr.davydovskyi@plus4u.net>
2024-03-14 20:43:49 +0100

    metadata change

--- server/controllers/iot-controller.js
+++ server/controllers/iot-controller.js
@@ -3,10 +3,12 @@ class IotController {
    async getIotData(req, res) {
       try {
          const iotData = req.body;
-         const encodedData = `${iotData?.data}`;
-         if (encodedData) {
+         if (iotData && iotData.data !== undefined) {
+            const encodedData = `${iotData.data}`;
             const decodedData = atob(encodedData);
             console.log(decodedData);
+         } else {
+            console.log("Metadata packet");
          }
 
          res.status(200).json(iotData);
