bbc4315c77269c169bfcd2ba85bf5f34ee4ff80a
Oleksandr Davydovskyi <oleksandr.davydovskyi@plus4u.net>
2024-03-14 13:22:58 +0100

    env file is not in gitignore now

--- server/index.js
+++ server/index.js
@@ -20,13 +20,12 @@ app.use(cors(
 
 app.use('/api', userRouter);
 app.use(errorMiddleware)
+console.log(process.env.DB_URL);
+console.log(typeof process.env.DB_URL);
 
 const start = async () => {
    try {
-      await mongoose.connect(process.env.DB_URL, {
-         useNewUrlParser: true,
-         useUnifiedTopology: true
-      });
+      await mongoose.connect(process.env.DB_URL);
       app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
    } catch (error) {
       console.log(error);
