c2dbcebb4cd6c85539737f9a6920e302efd557b9
Oleksandr Davydovskyi <oleksandr.davydovskyi@plus4u.net>
2024-03-14 15:16:00 +0100

    encode first try

--- server/controllers/iot-controller.js
+++ server/controllers/iot-controller.js
@@ -1,14 +1,17 @@
 class IotController {
 
-  async getIotData(req, res) {
-    try {
-      const iotData = req.body;
-      console.log(iotData);
-      res.status(200).json(iotData);
-    } catch (error) {
-      console.log(error);
-    }
-  }
+   async getIotData(req, res) {
+      try {
+         const iotData = req.body;
+         const encodedData = `${iotData.data}`;
+         const decodedData = atob(encodedData);
+
+         console.log(decodedData);
+         res.status(200).json(iotData);
+      } catch (error) {
+         console.log(error);
+      }
+   }
 }
 
 module.exports = new IotController();
