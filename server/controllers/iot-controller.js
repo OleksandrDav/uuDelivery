9f95b31bd2eebaf802f8d5b6016c68989a6884e5
Oleksandr Davydovskyi <oleksandr.davydovskyi@plus4u.net>
2024-04-04 16:15:39 +0200

    Add LPP decoding for IoT data

--- server/controllers/iot-controller.js
+++ server/controllers/iot-controller.js
@@ -1,19 +1,25 @@
-class IotController {
+const iotService = require('../services/iot-service');
+const { LPPDecoder } = require('lpp-node');
 
+class IotController {
    async getIotData(req, res) {
       try {
          const iotData = req.body;
+         let decodedData = {};
+
          if (iotData && iotData.data !== undefined) {
-            const encodedData = `${iotData.data}`;
-            const decodedData = atob(encodedData);
+            const lpp = new LPPDecoder();
+            const payload = Buffer.from(req.body.data, 'hex');
+            decodedData = lpp.decode(payload);
             console.log(decodedData);
          } else {
             console.log("Metadata packet");
          }
 
-         res.status(200).json(iotData);
+         res.status(200).json(decodedData);
       } catch (error) {
          console.log(error);
+         res.status(500).json({ error: 'Internal Server Error' });
       }
    }
 }
